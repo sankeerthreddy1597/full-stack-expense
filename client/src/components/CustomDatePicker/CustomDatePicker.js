b243b4a703e899f0a5773ebfb28c5b8aeabb1b06
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2021-06-21 13:36:30 -0400

    improved dataPicker code

--- client/src/components/CustomDatePicker/CustomDatePicker.js
+++ client/src/components/CustomDatePicker/CustomDatePicker.js
@@ -1,88 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./CustomDatePicker.css";
 import Moment from "react-moment";
 import moment from "moment";
 
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
-//TODO try to make this more efficient using map or forEach
 const CustomDatePicker = () => {
-  const date = new Date();
-  const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+  const [date, setDate] = useState(moment.now());
+  const datePickerRange = [-3, -2, -1, 0, 1, 2, 3];
+
+  useEffect(() => {}, [date]);
+
+  const prevDateClickHandler = () => {
+    setDate(moment(date).add(-1, "days"));
+  };
+
+  const nextDateClickHandler = () => {
+    setDate(moment(date).add(1, "days"));
+  };
   return (
     <>
       <div className="date-row">
-        <AiOutlineLeft color="#ababab" className="navDateIcon" size={26} />
-        <div className="date-component">
-          <p className="dayOfWeek">
-            {days[moment().add(-3, "days").isoWeekday() - 1]}
-          </p>
-          <p className="day">
-            <Moment subtract={{ days: 3 }} format="DD">
-              {date}
-            </Moment>
-          </p>
-        </div>
-        <div className="date-component">
-          <p className="dayOfWeek">
-            {days[moment().add(-2, "days").isoWeekday() - 1]}
-          </p>
-          <p className="day">
-            <Moment subtract={{ days: 2 }} format="DD">
-              {date}
-            </Moment>
-          </p>
-        </div>
-        <div className="date-component">
-          <p className="dayOfWeek">
-            {days[moment().add(-1, "days").isoWeekday() - 1]}
-          </p>
-          <p className="day">
-            <Moment subtract={{ days: 1 }} format="DD">
-              {date}
-            </Moment>
-          </p>
-        </div>
-        <div className="date-component">
-          <p className="dayOfWeek">{days[moment().isoWeekday() - 1]}</p>
-          <p className="day">
-            <Moment add={{ days: 0 }} format="DD">
-              {date}
-            </Moment>
-          </p>
-        </div>
-        <div className="date-component">
-          <p className="dayOfWeek">
-            {days[moment().add(1, "days").isoWeekday() - 1]}
-          </p>
-          <p className="day">
-            <Moment add={{ days: 1 }} format="DD">
-              {date}
-            </Moment>
-          </p>
-        </div>
-        <div className="date-component">
-          <p className="dayOfWeek">
-            {days[moment().add(2, "days").isoWeekday() - 1]}
-          </p>
-          <p className="day">
-            <Moment add={{ days: 2 }} format="DD">
-              {date}
-            </Moment>
-          </p>
-        </div>
-        <div className="date-component">
-          <p className="dayOfWeek">
-            {days[moment().add(3, "days").isoWeekday() - 1]}
-          </p>
-          <p className="day">
-            <Moment add={{ days: 3 }} format="DD">
-              {date}
-            </Moment>
-          </p>
-        </div>
+        <AiOutlineLeft
+          color="#ababab"
+          className="navDateIcon"
+          size={26}
+          onClick={prevDateClickHandler}
+        />
+        {datePickerRange.map((ele, index) => {
+          return (
+            <div className="date-component">
+              <p className="dayOfWeek">
+                <Moment add={{ days: ele }} format="ddd">
+                  {date}
+                </Moment>
+              </p>
+              <p className="day">
+                <Moment add={{ days: ele }} format="DD">
+                  {date}
+                </Moment>
+              </p>
+            </div>
+          );
+        })}
 
-        <AiOutlineRight color="#ababab" className="navDateIcon" size={26} />
+        <AiOutlineRight
+          color="#ababab"
+          className="navDateIcon"
+          size={26}
+          onClick={nextDateClickHandler}
+        />
       </div>
     </>
   );
