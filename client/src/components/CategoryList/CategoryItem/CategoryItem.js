5964c14244a304a9f44c6717ce3c8296e76c6941
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2021-07-17 19:12:19 -0400

    categoryList redux - amount and percent

--- client/src/components/CategoryList/CategoryItem/CategoryItem.js
+++ client/src/components/CategoryList/CategoryItem/CategoryItem.js
@@ -1,11 +1,32 @@
 import React from "react";
 import "./CategoryItem.css";
 
+import { useSelector } from "react-redux";
+
 const CategoryItem = ({ title, amount, percent }) => {
+  let totalAmount = 0;
+  let currAmount = 0;
+  let currPercent = 0;
+  const transactionDetails = useSelector(
+    (state) => state.transactionReducer.transactions
+  );
+
+  transactionDetails.map((transaction) => {
+    if (transaction.category === title.replace(/\s+/g, "")) {
+      currAmount += transaction.amount;
+    }
+    if (transaction.type !== "income") {
+      totalAmount += transaction.amount;
+    }
+    return totalAmount;
+  });
+
+  currPercent = ((currAmount / totalAmount) * 100).toFixed(2);
+
   const categoryColorMap = {
     foodAndBeverages: "#F5DD4122",
     groceries: "#F6A64222",
-    utilities: "#43C5F522",
+    Utilities: "#43C5F522",
     automobile: "#0D004E22",
     other: "#eeeeee",
   };
@@ -13,7 +34,7 @@ const CategoryItem = ({ title, amount, percent }) => {
   const barColorMap = {
     foodAndBeverages: "9px solid #F5DD41",
     groceries: "9px solid #F6A642",
-    utilities: "9px solid #43C5F5",
+    Utilities: "9px solid #43C5F5",
     automobile: " 9px solid #0D004E",
     other: "9px solid #777777",
   };
@@ -21,7 +42,7 @@ const CategoryItem = ({ title, amount, percent }) => {
   const percentColorMap = {
     foodAndBeverages: "#F5DD41",
     groceries: "#F6A642",
-    utilities: "#43C5F5",
+    Utilities: "#43C5F5",
     automobile: " #0D004E",
     other: "#777777",
   };
@@ -39,7 +60,7 @@ const CategoryItem = ({ title, amount, percent }) => {
         ></div>
         <div className="expenseSection">
           <p className="cat__title">{title}</p>
-          <p className="cat__amount">${amount}</p>
+          <p className="cat__amount">${currAmount}</p>
         </div>
       </div>
       <div className="catRight">
@@ -47,7 +68,7 @@ const CategoryItem = ({ title, amount, percent }) => {
           className="cat__percent"
           style={{ color: `${percentColorMap[title.replace(/\s+/g, "")]}` }}
         >
-          {percent}%
+          {currPercent}%
         </p>
       </div>
     </div>
