1a377b5f817b72c553b35a9069793461568258b2
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2021-07-17 20:16:51 -0400

    MultiUser state working, category css bug fixed

--- client/src/components/CategoryList/CategoryItem/CategoryItem.js
+++ client/src/components/CategoryList/CategoryItem/CategoryItem.js
@@ -26,7 +26,7 @@ const CategoryItem = ({ title, amount, percent }) => {
   const categoryColorMap = {
     foodAndBeverages: "#F5DD4122",
     groceries: "#F6A64222",
-    Utilities: "#43C5F522",
+    utilities: "#43C5F522",
     automobile: "#0D004E22",
     other: "#eeeeee",
   };
@@ -34,7 +34,7 @@ const CategoryItem = ({ title, amount, percent }) => {
   const barColorMap = {
     foodAndBeverages: "9px solid #F5DD41",
     groceries: "9px solid #F6A642",
-    Utilities: "9px solid #43C5F5",
+    utilities: "9px solid #43C5F5",
     automobile: " 9px solid #0D004E",
     other: "9px solid #777777",
   };
@@ -42,7 +42,7 @@ const CategoryItem = ({ title, amount, percent }) => {
   const percentColorMap = {
     foodAndBeverages: "#F5DD41",
     groceries: "#F6A642",
-    Utilities: "#43C5F5",
+    utilities: "#43C5F5",
     automobile: " #0D004E",
     other: "#777777",
   };
